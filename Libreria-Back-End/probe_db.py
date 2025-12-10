698e029b4000459d78442645084922af200d339f
Lobelt0 <137231293+Lobelt0@users.noreply.github.com>
2025-12-09 21:22:42 -0300

    Delete Libreria-Back-End directory

--- Libreria-Back-End/probe_db.py
+++
@@ -1,6 +0,0 @@
-from database import engine
-from sqlalchemy import text
-
-with engine.connect() as conn:
-    row = conn.execute(text("SELECT DATABASE(), 1")).fetchone()
-    print("Conectado a:", row)
